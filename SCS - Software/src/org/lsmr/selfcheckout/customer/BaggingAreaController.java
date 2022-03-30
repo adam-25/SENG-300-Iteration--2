c41146aefcf96c8290c273b421d0b2faf91aab79
FallenByGravity <34290834+FallenByGravity@users.noreply.github.com>
2022-03-30 13:54:23 -0600

    Add files via upload

--- SCS - Software/src/org/lsmr/selfcheckout/customer/BaggingAreaController.java
+++ SCS - Software/src/org/lsmr/selfcheckout/customer/BaggingAreaController.java
@@ -1,8 +1,14 @@
 package org.lsmr.selfcheckout.customer;
 
+import java.math.BigDecimal;
+
+import org.lsmr.selfcheckout.Barcode;
+import org.lsmr.selfcheckout.BarcodedItem;
+import org.lsmr.selfcheckout.Numeral;
 import org.lsmr.selfcheckout.devices.AbstractDevice;
 import org.lsmr.selfcheckout.devices.ElectronicScale;
 import org.lsmr.selfcheckout.devices.SelfCheckoutStation;
+import org.lsmr.selfcheckout.devices.SimulationException;
 import org.lsmr.selfcheckout.devices.observers.AbstractDeviceObserver;
 import org.lsmr.selfcheckout.devices.observers.ElectronicScaleObserver;
 
@@ -14,97 +20,107 @@ public class BaggingAreaController {
 	private ScanItemController scanItemControl;
 	private int numOfItemsInBaggingArea;
 	private double previousWeightOfCart;
-	
-	
-	//Constructor
+	private long begin;
+
+	// Constructor
 	public BaggingAreaController(SelfCheckoutStation cs) {
-		
+
 		checkoutStation = cs;
 		bac = new BAC();
 		weightOfCart = 0;
 		this.scanItemControl = null;
-		numOfItemsInBaggingArea =0;
-		
-		
-		//Register observers to the scanner
+		numOfItemsInBaggingArea = 0;
+
+		// Register observers to the scanner
 		checkoutStation.baggingArea.attach(bac);
-		
+
 	}
-	
-	//Connect bagging area control to scan item control
-	public void setScanItemControl(ScanItemController sIController)
-	{
+
+	// Connect bagging area control to scan item control
+	public void setScanItemControl(ScanItemController sIController) {
 		this.scanItemControl = sIController;
 	}
-	
+
 	public int getNumOfItemsInBaggingArea() {
 		return numOfItemsInBaggingArea;
 	}
-	
-	
-	
-	private class BAC implements ElectronicScaleObserver{
+
+	public void getBeginTime(long begin) {
+		this.begin = begin;
+	}
+
+	private class BAC implements ElectronicScaleObserver {
 		@Override
 		public void enabled(AbstractDevice<? extends AbstractDeviceObserver> device) {
 			// Ignore
-		
+
 		}
 
 		@Override
 		public void disabled(AbstractDevice<? extends AbstractDeviceObserver> device) {
 			// Ignore
-			
+
 		}
 
-		
 		@Override
 		public void weightChanged(ElectronicScale scale, double weightInGrams) {
 			previousWeightOfCart = weightOfCart;
 			weightOfCart = weightInGrams;
-			 
-			//Alter number of items in bagging area based on weight changed
-			if(weightOfCart > previousWeightOfCart) {
+
+			// Alter number of items in bagging area based on weight changed
+			if (weightOfCart > previousWeightOfCart) {
 				numOfItemsInBaggingArea++;
-			}else {
+			} else {
 				numOfItemsInBaggingArea--;
 			}
-			
-			//Once item has been placed in bagging area, enable the scanner
-			//If expected weight of cart (determined by scanner)
-			//Is the same of actual weigh of cart (determined by electronic scale)
-			if(scanItemControl.getWeightOfCart() == weightOfCart) {
+
+			// Once item has been placed in bagging area, enable the scanner
+			// If expected weight of cart (determined by scanner)
+			// Is the same of actual weigh of cart (determined by electronic scale)
+			if (scanItemControl.getWeightOfCart() == weightOfCart && weightOfCart
+					- scanItemControl.getWeightOfCart() < checkoutStation.baggingArea.getSensitivity()) {
 				checkoutStation.mainScanner.enable();
 				checkoutStation.handheldScanner.enable();
-			}else {
+			} else {
 				checkoutStation.mainScanner.disable();
 				checkoutStation.handheldScanner.disable();
 			}
-			
+
+			long end = System.currentTimeMillis();
+			if (end - begin > 5000) {
+				throw new SimulationException("Fail to place the item in the bagging area within the required time");
+			}
 		}
 
-		//Disable bar code scanner
+		// Disable bar code scanner
 		@Override
 		public void overload(ElectronicScale scale) {
 			checkoutStation.mainScanner.disable();
 			checkoutStation.handheldScanner.disable();
 		}
 
-		//Enable bar code scanner
+		// Enable bar code scanner
 		@Override
 		public void outOfOverload(ElectronicScale scale) {
-			checkoutStation.mainScanner.enable();	
+			checkoutStation.mainScanner.enable();
 			checkoutStation.handheldScanner.enable();
 		}
-		
 	}
-	
-	
-	
+
+	public void attendantVeritfyBag() {
+		BigDecimal bagPrice = new BigDecimal(0);
+		Numeral[] nBag = { Numeral.one, Numeral.two, Numeral.three, Numeral.four };
+		Barcode barcodeBag = new Barcode(nBag);
+		scanItemControl.getBarcodePrice().put(barcodeBag, bagPrice);
+		double bagWeight = weightOfCart - previousWeightOfCart;
+		scanItemControl.getBarcodeWeight().put(barcodeBag, bagWeight);
+		BarcodedItem bagItem = new BarcodedItem(barcodeBag, bagWeight);
+		checkoutStation.mainScanner.enable();
+		checkoutStation.mainScanner.scan(bagItem);
+	}
+
 	public double getWeightOfCart() {
 		return weightOfCart;
 	}
-	
-	
-	
 
 }
--- SCS - Software/src/org/lsmr/selfcheckout/customer/PaymentController.java
+++ SCS - Software/src/org/lsmr/selfcheckout/customer/PaymentController.java
@@ -7,8 +7,11 @@ import java.util.Currency;
 import java.util.List;
 
 import org.lsmr.selfcheckout.BlockedCardException;
+import org.lsmr.selfcheckout.Card;
 import org.lsmr.selfcheckout.Coin;
+import org.lsmr.selfcheckout.customer.TouchScreenController.checkoutState;
 import org.lsmr.selfcheckout.Card.CardData;
+import org.lsmr.selfcheckout.Card.CardSwipeData;
 import org.lsmr.selfcheckout.devices.AbstractDevice;
 import org.lsmr.selfcheckout.devices.BanknoteSlot;
 import org.lsmr.selfcheckout.devices.BanknoteValidator;
@@ -25,48 +28,55 @@ import org.lsmr.selfcheckout.devices.observers.CoinSlotObserver;
 import org.lsmr.selfcheckout.devices.observers.CoinTrayObserver;
 import org.lsmr.selfcheckout.devices.observers.CoinValidatorObserver;
 
+public class PaymentController extends TouchScreenController{
 
-public class PaymentController{
-
+	// The three possible values CardDate.getType() should return
+	private final String debit = "DEBIT";
+	private final String credit = "CREDIT";
+	private final String membership = "MEMBERSHIP";
+	
+	// Used for testing. Setting verified to false will simulate the bank rejecting the credit/debit card.
+	public boolean verified = true;
+	
+	
+	
 	private BigDecimal valueOfCart;
 	private final SelfCheckoutStation checkoutStation; 
+	///private TouchScreenController touchScreen = new TouchScreenController(checkoutStation);
 	private PCC pcc;
 	private PCB pcb;
 	private CC cc;
 	private List<Coin> coinTrayList;
 	private BigDecimal initialValueOfCart;
-	private final String debit = "DEBIT";
-	private final String credit = "CREDIT";
-	private final String membership = "MEMBERSHIP";
-	public boolean verified = true;
 	private String membershipNo = null;
+	private boolean showError = false;
+	private boolean cardDataRead = false;
+	private boolean cardInsert = false;
 	
-	
-	
-	//Customer checkout use case 
 	public PaymentController(SelfCheckoutStation cs){
+		super(cs);
 		checkoutStation = cs;
+		//touchScreen = new TouchScreenController(checkoutStation);
 		initialValueOfCart = new BigDecimal(0);
 		valueOfCart = new BigDecimal(0);
 		coinTrayList = new ArrayList<Coin>();
 		
-		
-		//Initializing observers
+		// Initializing observers
 		pcc = new PCC();
 		pcb = new PCB();
 		cc = new CC();
 		
-		//Register observers in the coin related devices
+		// Register observers in the coin related devices
 		checkoutStation.coinSlot.attach(pcc);
 		checkoutStation.coinValidator.attach(pcc);
 		checkoutStation.coinTray.attach(pcc);
 		
-		//Registers observers in the bank note related devices
+		// Registers observers in the bank note related devices
 		checkoutStation.banknoteInput.attach(pcb);
 		checkoutStation.banknoteValidator.attach(pcb);
 		checkoutStation.banknoteInput.attach(pcb);
 		
-		//Registers observers in the Card related devices
+		// Registers observers in the Card related devices
 		 checkoutStation.cardReader.attach(cc);
 	}
 	
@@ -83,12 +93,35 @@ public class PaymentController{
 		valueOfCart = cartValue;
 	}
 	
-	//this method is used when an invalid card is read, in the final implementation, an error would
-	//be displayed on the customer's screen and then would prompt him to select a payment option
+	public boolean getShowError()
+	{
+		return showError;
+	}
+	
+	public boolean getCardInserted()
+	{
+		return cardInsert;
+	}
+	
+	/**
+	 * This method is used when an invalid card is read. In the final implementation, an error would
+	 * be displayed on the customer's screen, then they would get prompted to choose a payment option.
+	*/
 	public void displayError() 
 	{
-		System.out.println("an error has occured");
-		//go back to payment options
+		System.out.println("an error has occurred");
+		showError = true;
+		// The user would get sent back to the payment options screen in the final implementation
+	}
+	
+	public String getMembershipNo()
+	{
+		return membershipNo;
+	}
+	
+	public boolean getCardData()
+	{
+		return cardDataRead;
 	}
 	
 	public boolean hasMembership()
@@ -101,8 +134,28 @@ public class PaymentController{
 		return true;
 	}
 	
-	//If all items have been paid for, return true
-	//And disable the coin and bank note slot
+	public void manualMembershipEntry(String manualMembershipNo) {
+		// Simulates customer entering their membership number through touch screen.
+//		manualMembershipNo = "405200";
+		/**
+		 * Simulate going to the database and finding which account corresponds with
+		 * the entered Membership number
+		*/
+		membershipNo = manualMembershipNo;
+	}
+	
+	public void addItemsWithPartialPayment() {
+		state = checkoutState.SCAN;
+		checkoutStation.mainScanner.enable();
+		checkoutStation.handheldScanner.enable();
+		checkoutStation.coinSlot.disable();
+		checkoutStation.banknoteInput.disable();
+	}
+	
+	/**
+	 * If all items have been paid for, return true
+	 * And disable the coin and bank note slot.
+	*/
 	public boolean isAllItemPaid() {
 		if (valueOfCart.compareTo(new BigDecimal(0)) == -1 || valueOfCart.compareTo(new BigDecimal(0)) == 0 ) {
 			checkoutStation.coinSlot.disable();
@@ -113,10 +166,10 @@ public class PaymentController{
 		return false;
 	}
 	
-
 	public List<Coin> getCoinTrayList() {
 		return coinTrayList;
 	}
+	
 	
 	//COIN PAYMENT - Implementation of Coin observers
 	private class PCC implements CoinSlotObserver, CoinValidatorObserver, CoinTrayObserver{
@@ -150,7 +203,6 @@ public class PaymentController{
 		
 		@Override
 		public void coinAdded(CoinTray tray) {
-			
 			//Simulates removal of coin from the coin tray
 			for(Coin theCoin :tray.collectCoins() ) {
 				coinTrayList.add(theCoin);
@@ -160,11 +212,8 @@ public class PaymentController{
 	}
 	
 	
-	
-	
 	//BANKNOTE PAYMENT - Implementation of Bank note observers
-	private class PCB implements BanknoteSlotObserver, BanknoteValidatorObserver{
-		
+	private class PCB implements BanknoteSlotObserver, BanknoteValidatorObserver{	
 		@Override
 		public void enabled(AbstractDevice<? extends AbstractDeviceObserver> device) {			
 			//Ignore
@@ -205,139 +254,163 @@ public class PaymentController{
 		}
 	}
 	
-	private class CC implements CardReaderObserver {
-		
-		public boolean verifyCVV(String data)
-		{
-			return false;
-		}
-		public boolean verifyCardNumber(String data)
-		{
-			return false;
-		}
-		
-		public boolean verifyDebitCard(CardData data)
-		{
-			return verified;
-		}
-		
-		public boolean verifyCreditCard(CardData data)
-		{
-			return verified;
-		}
-		
-		public boolean verifyMembershipCard(CardData data)
-		{
-			return verified;
-		}
 	
+	//CARD CONTROLLER - Implementation of CardReader observers
+	private class CC implements CardReaderObserver {
+		/**
+		 * Checks to make sure the CVV is of length 3 and only contains digits
+		*/
+		public boolean verifyCVV(String data) {
+			if(data.matches("[0-9]+") && data.length() ==  3) {
+				return true;
+			}
+			return false;
+		}
+		
+		/**
+		 * Checks to make sure the Card Number is of length 16 and only contains digits
+		*/
+		public boolean verifyCardNumber(String data) {
+			if(data.matches("[0-9]+") && data.length() ==  16) {
+				return true;
+			}
+			return false;
+		}
+		
+		// Simulates verifying a debit card with the bank. Returns verified.
+		public boolean verifyDebitCard(CardData data) {
+			return verified;
+		}
+		
+		// Simulates verifying a credit card with the bank. Returns verified.
+		public boolean verifyCreditCard(CardData data) {
+			return verified;
+		}
+		
+		// Simulates verifying a membership card with the database containing membership info. Returns verified.
+		public boolean verifyMembershipCard(CardData data) {
+			return verified;
+		}
 
 		@Override
 		public void enabled(AbstractDevice<? extends AbstractDeviceObserver> device) {
 			// ignore
-			
 		}
 
 		@Override
 		public void disabled(AbstractDevice<? extends AbstractDeviceObserver> device) {
 			// ignore
-			
 		}
 
 		@Override
 		public void cardInserted(CardReader reader) {
-			// ignore 
-			
+			cardInsert = true;
 		}
 
 		@Override
 		public void cardRemoved(CardReader reader) {
-			// ignore - membership cards can only be swiped or have number manually entered.
-			
+			cardInsert = false;
 		}
 
 		@Override
 		public void cardTapped(CardReader reader) {
-			// ignore - membership cards can only be swiped or have number manually entered.
-			
+			System.out.println("Reading card data. Please wait...");
 		}
 
 		@Override
 		public void cardSwiped(CardReader reader) {
 			System.out.println("Reading card data. Please wait...");
-		
-			
 		}
 
+		/**
+		 * Method is called whenever a card of any type is swiped, inserted or tapped by the CardReader
+		*/
 		@Override
-		public void cardDataRead(CardReader reader, CardData data){
+		public void cardDataRead(CardReader reader, CardData data) {
+			cardDataRead = true;
 			String cardType = data.getType();
 			String cardNumber = data.getNumber();
 			String cardHolder = data.getCardholder();
-			String cardCVV = data.getCVV();
+			String cardCVV = null;
 			
-			if(cardType == null)
-			{
-					displayError();
+			// If the card was swiped you cannot get the CVV information
+			if (!(data instanceof CardSwipeData)) {
+			cardCVV = data.getCVV();
 			}
 			
-			if(cardType == debit)
-			{
-				//review try catch logic
-				if(verifyCardNumber(cardNumber) == false || cardHolder == null || verifyCVV(cardCVV) == false)
-				{
-					displayError();
-				}
+			
+			// We don't need it because if CardType is null then SimulationException will be directly thrown.
+			
+			
+			if(cardType == debit) {
+				// Card was tapped or inserted. Need to verify CVV
+				if (!(data instanceof CardSwipeData)) {
+					if(verifyCardNumber(cardNumber) == true && cardHolder != null && verifyCVV(cardCVV) == true) {
+						if(verifyDebitCard(data) == true) {
+							valueOfCart = new BigDecimal(0);
+							isAllItemPaid();
+						} else {
+							displayError();
+						}
+					} else {
+						displayError();
+					}
 				
-				if(verifyDebitCard(data) == true)
-				{
-					valueOfCart = new BigDecimal(0);
-					isAllItemPaid();
+				// Card was swiped. Verify only card name and number
+				} else {
+					if(verifyCardNumber(cardNumber) == true && cardHolder != null) {
+						if(verifyDebitCard(data) == true) {
+							valueOfCart = new BigDecimal(0);
+							isAllItemPaid();
+						} else {
+							displayError();
+						}
+					} else {
+						displayError();
+					}
 				}
-				else {
-					displayError();
-				}
-				
 			}
 			
-			else if(cardType == credit)
-			{
-				//review try catch logic
-				if(verifyCardNumber(cardNumber) == false || cardHolder == null || verifyCVV(cardCVV) == false)
-				{
-					displayError();
-				}
+			else if(cardType == credit) {
+				// Card was tapped or inserted. Need to verify CVV
+				if (!(data instanceof CardSwipeData)) {
+					if(verifyCardNumber(cardNumber) == true && cardHolder != null && verifyCVV(cardCVV) == true) {
+						if(verifyDebitCard(data) == true) {
+							valueOfCart = new BigDecimal(0);
+							isAllItemPaid();
+						} else {
+							displayError();
+						}
+					} else {
+						displayError();
+					}
 				
-				if(verifyCreditCard(data) == true)
-				{
-					valueOfCart = new BigDecimal(0);
-					isAllItemPaid();
-				}
-				else {
-					displayError();
-				}
-
-				
-			
+				// Card was swiped. Verify only card name and number
+				} else {
+					if(verifyCardNumber(cardNumber) == true && cardHolder != null) {
+						if(verifyCreditCard(data) == true) {
+							valueOfCart = new BigDecimal(0);
+							isAllItemPaid();
+						} else {
+							displayError();
+						}
+					} else {
+						displayError();
+					}
+				}				
 			}
 			
-			else if(cardType == membership)
-			{
-				if(cardHolder == null || verifyCardNumber(cardNumber) == false)
-				{
+			else if(cardType == membership){
+				// Change || to && Need to check.
+				// Membership cards are always swiped
+				if(cardHolder != null && verifyCardNumber(cardNumber) == true) {
+					if(verifyMembershipCard(data) == true) {
+						membershipNo = cardNumber;
+					}
+				} else {
 					displayError();
 				}
-				if(verifyMembershipCard(data) == true)
-				{
-					membershipNo = cardNumber;
-				}
-				
-				
 			}
-			
 		}
-		
-		
 	}
-	
 }
